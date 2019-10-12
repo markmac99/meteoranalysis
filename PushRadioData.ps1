b3014749419180137831c61e10c914ce04173f6b
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2019-10-12 13:01:17 +0100

    Added rbmob file to push

--- PushRadioData.ps1
+++ PushRadioData.ps1
@@ -1,3 +1,9 @@
+cd c:\spectrum\screenshots
+scp -i C:\Users\Mark\Documents\projects\aws\markskey.pem ..\latest2d.jpg ec2-user@ec2-18-130-54-182.eu-west-2.compute.amazonaws.com:/var/www/html/markmcintyreastro/meteors/radio
 $fnam=(ls  c:\spectrum\screenshots\ | sort-object lastwritetime).fullname | select-object -last 1
-aws s3 cp $fnam s3://mjmm-meteor-uploads
-aws s3 cp c:\spectrum\latest2d.jpg s3://mjmm-meteor-uploads
+scp -i C:\Users\Mark\Documents\projects\aws\markskey.pem $fnam ec2-user@ec2-18-130-54-182.eu-west-2.compute.amazonaws.com:/var/www/html/markmcintyreastro/meteors/radio/latestcapture.jpg
+$mmyyyy=((get-date).tostring("MMyyyy"))
+$fnam='C:\Program Files (x86)\Colorgramme Lab\img\McIntyre_'+ $mmyyyy +'.jpg'
+scp -i C:\Users\Mark\Documents\projects\aws\markskey.pem $fnam ec2-user@ec2-18-130-54-182.eu-west-2.compute.amazonaws.com:/var/www/html/markmcintyreastro/meteors/radio/latestcapture.jpg
+ssh -i C:\Users\Mark\Documents\projects\aws\markskey.pem ec2-user@ec2-18-130-54-182.eu-west-2.compute.amazonaws.com /var/www/html/markmcintyreastro/meteors/getdata.sh
+
