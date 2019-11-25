e8b5b82285f4c6f670c91f83d66e050279c57bcf
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2019-11-25 21:46:23 +0000

    added colorgram creation

--- getall.ps1
+++ getall.ps1
@@ -1,9 +1,13 @@
-﻿cd $PSScriptRoot
+﻿set-location $PSScriptRoot
 invoke-expression -Command .\get_files-TC.ps1
-cd $PSScriptRoot
+set-location $PSScriptRoot
 invoke-expression -Command .\get_files-NE.ps1
-cd $PSScriptRoot
+set-location $PSScriptRoot
 invoke-expression -Command .\get_files-GMN1.ps1
-cd $PSScriptRoot
+set-location $PSScriptRoot
 invoke-expression -Command .\get_files-RADIO.ps1
+set-location $PSScriptRoot
+invoke-expression -command .\create-colorgram.bat
+
+
 
