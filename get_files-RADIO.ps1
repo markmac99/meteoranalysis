95a301a0101938e9e325aa6f627b6fea3b6381ef
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2019-12-21 22:53:41 +0000

    add code to create empty rmob file

--- get_files-RADIO.ps1
+++ get_files-RADIO.ps1
@@ -14,6 +14,12 @@ net use \\markslaptop\spectrum  /d
 net use \\markslaptop\colorgramme /user:radiometeor Radiometeor3
 robocopy \\markslaptop\colorgramme\rmob *.* rmob 
 robocopy \\markslaptop\colorgramme\img *.* rmob
+
+# create next month's empty RMOB file, if it doesn't already exist
+$nexmth=(get-date).adddays(8).tostring("yyyyMM")
+$fname = -join("\\markslaptop\spectrum\RMOB-", $nexmth, ".DAT")
+if((test-path $fname) -eq $false) { new-item -path $fname }
+
 net use \\markslaptop\colorgramme /d
 cd $x
 
