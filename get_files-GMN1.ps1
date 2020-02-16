511d453099f86bfb0e790758108351d397d920ce
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2020-02-16 14:36:05 +0000

    Better logging and creating consol on Pi

--- get_files-GMN1.ps1
+++ get_files-GMN1.ps1
@@ -1,7 +1,10 @@
 ﻿$logf=  -join("..\logs\robocopy-gmn1-", (get-date -uformat "%Y%m%d-%H%M%S"),".log")
 
 Write-Output "starting" (get-date) 
+$msg = "starting " + (get-date)  
+add-content $logf $msg
 c:
+$curdir=get-location
 set-location C:\Users\Mark\Videos\Astro\MeteorCam\UK0006
 
 $loopctr=0
@@ -13,7 +16,7 @@ while (($? -ne "True") -and ($loopctr -lt 10))  {
 }
 if ($loopctr -eq 10)  {
     Send-MailMessage -from meteorpi@oservatory -to mark@localhost -subject "pi camera down" -body "rpicamera seems to be down, check power and network" -smtpserver 192.168.1.151    
-    Write-Output "pi camera seems to be down, check power and network" > $logf
+    Write-Output "pi camera seems to be down, check power and network" >> $logf
     exit 1
 }
 
@@ -29,7 +32,7 @@ if ($chk -ne "True")
             Send-MailMessage -from rpicamera@oservatory -to mark@localhost -subject "camera down" -body "camera seems to be down, check power and network" -smtpserver 192.168.1.151    
             Write-Output "camera down" > $pidown
         }
-        Write-Output "Pi camera seems to be down, check power and network" > $logf
+        add-content $logf  "Pi camera seems to be down, check power and network`n" 
         exit 1
     }
 } 
@@ -37,18 +40,30 @@ if ([System.IO.File]::Exists($pidown))
 {
     remove-item $pidown
 }
-net use \\meteorpi\RMS_share
+net use \\meteorpi\RMS_share /user:pi Wombat33rpi
 if ($? -ne "True")  {
     Send-MailMessage -from rpicamera@observatory -to mark@localhost -subject "rpicamera: Unable co connect" -body "unable to connect to rpicamera" -smtpserver 192.168.1.151    
     Add-Content $logf "net-use failed`n"
     exit 2
 } 
 write-output "updating ConfirmedFiles"
-ssh -o StrictHostKeyChecking=no -i C:\cygwin\home\mark\.ssh\pikey.pub pi@meteorpi ~/redoConfirmed.sh
+add-content $logf "updating ConfirmedFiles`n"
+if ($env:username -eq 'admin' )
+{
+    $keyf='c:\users\admin\.ssh\pikey.ppk'
+}else{
+    $keyf='C:\users\mark\.ssh\pikey.ppk'
+}
+ssh -o StrictHostKeyChecking=no -i $keyf  pi@meteorpi ~/redoConfirmed.sh
 
 Write-Output "copying data" 
+add-content $logf "copying data"
 robocopy \\meteorpi\rms_share\ArchivedFiles ArchivedFiles /dcopy:DAT /tee /v /s /r:3 /log+:$logf
 robocopy \\meteorpi\rms_share\ConfirmedFiles ConfirmedFiles /dcopy:DAT /tee /v /s /r:3 /log+:$logf
 
 net use \\meteorpi\RMS_share /d
+set-location $curdir
+
+& $PSScriptRoot\picamera\Get-interesting-pidata.ps1
 Write-Output "finished" (get-date) 
+add-content $logf "finished"
