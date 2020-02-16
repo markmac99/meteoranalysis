628bfde0250b1a21fa4b5227c101cb7e7f1df21d
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2020-02-16 14:37:06 +0000

    pointed to new archive

--- ukmon-archive/get-archive.ps1
+++ ukmon-archive/get-archive.ps1
@@ -1,10 +1,11 @@
 # script to get CSV files from UFO Archive
 
-$keyfile='C:\Users\mark\Documents\Projects\Certs\ukmon-key.csv'
+$keyfile='C:\Users\mark\Documents\Projects\aws\ukmon-shared.csv'
 $keys=((Get-Content $keyfile)[1]).split(',')
 
 $Env:AWS_ACCESS_KEY_ID = $keys[0]
 $env:AWS_SECRET_ACCESS_KEY = $keys[1]
 
-aws s3 sync s3://ukmon-shared/archive/ c:/users/mark/videos/astro/MeteorCam/archive/ --exclude "*" --include "*.csv"
+#aws s3 sync s3://ukmon-shared/archive/ c:/users/mark/videos/astro/MeteorCam/archive/ --exclude "*" --include "*.csv"
+aws s3 sync s3://ukmon-shared/consolidated c:/users/mark/videos/astro/MeteorCam/archive/ --exclude "*" --include "*.csv" --exclude "*temp*"
 
