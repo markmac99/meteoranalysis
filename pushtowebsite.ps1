0407de9748c0d37ec0cac28991712d5f2b780155
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2020-03-12 16:44:32 +0000

    added capability to run as different users

--- pushtowebsite.ps1
+++ pushtowebsite.ps1
@@ -5,6 +5,13 @@ $yr = (get-date -uformat '%Y')
 $mth = (get-date -uformat '%Y%m')
 $awssite=get-content 'c:\users\mark\videos\astro\meteorcam\radio\awssite.txt'
 $usr= 'bitnami@'+$awssite
+if ($env:username -eq 'admin' )
+{
+    $key='c:\users\admin\.ssh\markskey.pem'
+}else{
+    $key='C:\users\mark\.ssh\markskey.pem'
+}
+
 
 write-output 'Collecting TC data'
 set-location c:\users\mark\videos\astro\meteorcam\tc\$yr\$mth
@@ -14,7 +21,6 @@ $x=(get-childitem -r *p.jpg).fullname
 for ($i=0;$i -lt $x.count ; $i++) {copy-item $x[$i] c:\temp\tc }
 
 write-output 'pushing to TC folder'
-$key='c:\users\mark\.ssh\markskey.pem'
 $targ= 'bitnami@'+$awssite+':data/meteors/tc/' 
 ssh -o StrictHostKeyChecking=no -i $key $usr rm -f data/meteors/tc/*.jpg
 scp -o StrictHostKeyChecking=no -i $key c:\temp\tc\*.jpg $targ
@@ -27,7 +33,6 @@ $x=(get-childitem -r *p.jpg).fullname
 for ($i=0;$i -lt $x.count ; $i++) {copy-item $x[$i] c:\temp\ne }
 
 write-output 'pushing to NE folder'
-$key='c:\users\mark\.ssh\markskey.pem'
 $targ= 'bitnami@'+$awssite+':data/meteors/ne/' 
 ssh -o StrictHostKeyChecking=no -i $key $usr rm -f data/meteors/ne/*.jpg
 scp -o StrictHostKeyChecking=no -i $key c:\temp\ne\*.jpg $targ
