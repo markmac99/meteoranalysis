6f4996f13cce3f734016a51d6764cfd60beadd3f
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2020-03-29 14:23:22 +0100

    script to create stack from stills

---
+++ makeStack.ps1
@@ -0,0 +1,29 @@
+# script to push video data to the website
+$curloc = get-location
+$fnam=$args[0]
+
+$awssite=get-content 'c:\users\mark\videos\astro\meteorcam\radio\awssite.txt'
+$usr= 'bitnami@'+$awssite
+if ($env:username -eq 'admin' )
+{
+    $key='c:\users\admin\.ssh\markskey.pem'
+}else{
+    $key='C:\users\mark\.ssh\markskey.pem'
+}
+
+
+write-output 'Collecting data'
+set-location c:\users\mark\videos\astro\meteorcam\tmpstack
+
+write-output 'pushing to temp folder'
+$targ= 'bitnami@'+$awssite+':data/meteors/tmp/' 
+ssh -o StrictHostKeyChecking=no -i $key $usr rm -f data/meteors/tmp/*.jpg
+scp -o StrictHostKeyChecking=no -i $key *.jpg $targ
+
+write-output 'Creating stack'
+ssh -o StrictHostKeyChecking=no -i $key $usr data/meteors/tmp/makestack.sh
+
+scp -o StrictHostKeyChecking=no -i $key $targ/stack.jpg .
+move stack.jpg ../stacks/$fnam.jpg
+
+set-location $curloc
