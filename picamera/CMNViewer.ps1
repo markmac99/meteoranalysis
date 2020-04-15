a4b76302e32f4a3e063f2a04629993c0fec30dda
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2020-04-15 21:07:47 +0100

    misc changes to website and cmn confirmation proc

--- picamera/CMNViewer.ps1
+++ picamera/CMNViewer.ps1
@@ -5,5 +5,8 @@ net use \\meteorpi\RMS_share /user:pi Wombat33rpi
 $rootdir='\\meteorpi\RMS_Share\ArchivedFiles'
 $path=(get-childitem $rootdir -directory | sort-object creationtime | select-object -last 1).name
 $myf = $rootdir + '\'+$path
-set-location 'C:\Program Files (x86)\CMN_binViewer'
-& .\CMN_binViewer.exe $myf
+#set-location 'C:\Program Files (x86)\CMN_binViewer'
+#& .\CMN_binViewer.exe $myf
+set-location \\buffalonas\mark\projects\CroatianMeteorNetwork\cmn_binviewer
+conda activate binviewer
+python CMN_BinViewer.py $myf
--- pushtowebsite.ps1
+++ pushtowebsite.ps1
@@ -15,27 +15,27 @@ if ($env:username -eq 'admin' )
 
 write-output 'Collecting TC data'
 set-location c:\users\mark\videos\astro\meteorcam\tc\$yr\$mth
-mkdir  C:\temp\tc 2> $NULL
-remove-item c:\temp\tc\*.jpg
+mkdir  c:\users\mark\videos\astro\meteorcam\temp\tc 2> $NULL
+remove-item c:\users\mark\videos\astro\meteorcam\temp\tc\*.jpg
 $x=(get-childitem -r *p.jpg).fullname
-for ($i=0;$i -lt $x.count ; $i++) {copy-item $x[$i] c:\temp\tc }
+for ($i=0;$i -lt $x.count ; $i++) {copy-item $x[$i] c:\users\mark\videos\astro\meteorcam\temp\tc }
 
 write-output 'pushing to TC folder'
 $targ= 'bitnami@'+$awssite+':data/meteors/tc/' 
 ssh -o StrictHostKeyChecking=no -i $key $usr rm -f data/meteors/tc/*.jpg
-scp -o StrictHostKeyChecking=no -i $key c:\temp\tc\*.jpg $targ
+scp -o StrictHostKeyChecking=no -i $key c:\users\mark\videos\astro\meteorcam\temp\tc\*.jpg $targ
 
 write-output 'Collecting NE data'
 set-location c:\users\mark\videos\astro\meteorcam\ne\$yr\$mth
-mkdir C:\temp\ne 2> $NULL
-remove-item c:\temp\ne\*.jpg
+mkdir c:\users\mark\videos\astro\meteorcam\temp\ne 2> $NULL
+remove-item c:\users\mark\videos\astro\meteorcam\temp\ne\*.jpg
 $x=(get-childitem -r *p.jpg).fullname
-for ($i=0;$i -lt $x.count ; $i++) {copy-item $x[$i] c:\temp\ne }
+for ($i=0;$i -lt $x.count ; $i++) {copy-item $x[$i] c:\users\mark\videos\astro\meteorcam\temp\ne }
 
 write-output 'pushing to NE folder'
 $targ= 'bitnami@'+$awssite+':data/meteors/ne/' 
 ssh -o StrictHostKeyChecking=no -i $key $usr rm -f data/meteors/ne/*.jpg
-scp -o StrictHostKeyChecking=no -i $key c:\temp\ne\*.jpg $targ
+scp -o StrictHostKeyChecking=no -i $key c:\users\mark\videos\astro\meteorcam\temp\ne\*.jpg $targ
 
 write-output 'Creating stacks'
 ssh -o StrictHostKeyChecking=no -i $key $usr data/meteors/update.sh
@@ -43,7 +43,7 @@ ssh -o StrictHostKeyChecking=no -i $key $usr data/meteors/update.sh
 write-output 'finding latest RMS stack'
 $targ= 'bitnami@'+$awssite+':data/meteors/' 
 $x=(get-childitem -r c:\users\mark\videos\astro\meteorcam\UK0006\ConfirmedFiles\*stack*.jpg).FullName
-copy-item $x[$x.count-1] c:\temp\UK0006_latest.jpg
-scp -o StrictHostKeyChecking=no -i $key c:\temp\UK0006_latest.jpg $targ
+copy-item $x[$x.count-1] c:\users\mark\videos\astro\meteorcam\temp\UK0006_latest.jpg
+scp -o StrictHostKeyChecking=no -i $key c:\users\mark\videos\astro\meteorcam\temp\UK0006_latest.jpg $targ
 
 set-location $curloc
