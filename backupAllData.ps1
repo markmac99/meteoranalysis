617857d0e51e25b4795c7b8d2d32cc3f143b1be4
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2020-05-26 17:29:38 +0100

    New backup script

---
+++ backupAllData.ps1
@@ -0,0 +1,24 @@
+# powershell script to backup the meteorcam data
+
+$src='C:\Users\mark\Videos\astro\MeteorCam'
+$dest='g:\astrovideo\meteorcam'
+$cl='ne','tc','uk0006'
+
+$CurrentDate = Get-Date
+$daysback=-190
+$DatetoDelete = $CurrentDate.AddDays($Daysback)
+
+$yr = [int](get-date -uformat '%Y')
+$syr=$yr-1
+
+foreach ($cam in $cl)
+{
+    for ($i=$syr; $i -le $yr; $i++ )
+    {
+        $ssrc=$src+'\'+$cam+'\'+$i
+        $sdest=$dest+'\'+$cam+'\'+$i 
+        echo $ssrc $sdest
+        robocopy $ssrc $sdest *.csv *.jpg *.bmp *.txt *.xml M*.avi /dcopy:DAT /m /v /s /r:3
+        Get-ChildItem $ssrc -Recurse | Where-Object { $_.LastWriteTime -lt $DatetoDelete } | Remove-Item -recurse
+    }
+}
