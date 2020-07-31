2eb96266b8fe662e9ed5de998d0f5e120fefcce2
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2020-07-31 18:52:50 +0100

    added curation of data to the getall script

--- getall.ps1
+++ getall.ps1
@@ -10,5 +10,13 @@ set-location $PSScriptRoot
 if ((get-date).hour -eq 8 ) {invoke-expression -command .\ukmon-archive\get-archive.ps1}
 set-location $PSScriptRoot
 if ((get-date).hour -eq 20 ) {invoke-expression -command .\pushtowebsite.ps1}
+set-location $PSScriptRoot
+if ((get-date).hour -eq 12 ) {
+    set-location c:\users\mark\documents\projects\meteorhunting\ukmon-shared\newanalysis
+    $dt=get-date -uformat '%Y%m%d'
+    conda activate RMS
+    python curateCamera.py ..\tackley_tc $dt
+    python curateCamera.py ..\tackley_ne $dt
+}
 
 
